c37b13adb449174bcdfda20793884e01c61fcb1c
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-06-04 14:18:07 -0700

    Refactoring tests to incorporate a more "table" structure.

--- goBook/testing/joinWithCommas.go
+++ goBook/testing/joinWithCommas.go
@@ -14,12 +14,31 @@ func BuildJoin() {
 
 func joinWithCommas(phrases []string) string {
 	//Appending commas to the string
-	if len(phrases) == 2 {
+
+	switch len(phrases) {
+	case 0:
+		return ""
+	case 1:
+		return phrases[0]
+	case 2:
 		return phrases[0] + " and " + phrases[1]
-	} else {
+	default:
 		result := strings.Join(phrases[:len(phrases)-1], ", ")
 		result += ", and "
 		result += phrases[len(phrases)-1]
 		return result
 	}
+
+	//if len(phrases) == 0 {
+	//	return ""
+	//} else if len(phrases) == 1 {
+	//	return phrases[0]
+	//} else if len(phrases) == 2 {
+	//	return phrases[0] + " and " + phrases[1]
+	//} else {
+	//	result := strings.Join(phrases[:len(phrases)-1], ", ")
+	//	result += ", and "
+	//	result += phrases[len(phrases)-1]
+	//	return result
+	//}
 }
--- goBook/testing/joinWithCommas_test.go
+++ goBook/testing/joinWithCommas_test.go
@@ -5,36 +5,67 @@ import (
 	"testing"
 )
 
+type testData struct {
+	list []string
+	want string
+}
+
 func TestBuildJoin(t *testing.T) {
-	//t.Error("no test written yet")
-}
-
-func TestTwoElements(t *testing.T) {
-	list := []string{"apple", "orange"}
-
-	want := "apple and orange"
-	got := joinWithCommas(list)
-
-	if got != want {
-		t.Error(errorString(list, got, want))
+	tests := []testData{
+		{list: []string{}, want: ""},
+		{list: []string{"apple"}, want: "apple"},
+		{list: []string{"apple", "orange"}, want: "apple and orange"},
+		{list: []string{"apple", "orange", "peach"}, want: "apple, orange, and peach"},
 	}
 
-	//t.Error("no test written yet")
-}
-
-func TestThreeElements(t *testing.T) {
-	//t.Error("no test written yet")
-
-	list := []string{"apple", "orange", "peach"}
-
-	want := "apple, orange, and peach"
-	got := joinWithCommas(list)
-
-	if got != want {
-		t.Error(errorString(list, got, want))
+	for _, test := range tests {
+		got := joinWithCommas(test.list)
+		if got != test.want {
+			t.Errorf("\n got: joinWithCommas(%#v) = \"%s\" \n want: \"%s\"", test.list, got, test.want)
+		}
 	}
-
+	//t.Error("no test written yet")
 }
+
+//func TestOneElement(t *testing.T) {
+//	list := []string{"apple"}
+//
+//	want := "apple"
+//	got := joinWithCommas(list)
+//
+//	if got != want {
+//		t.Error(errorString(list, got, want))
+//	}
+//
+//	//t.Error("no test written yet")
+//}
+//
+//func TestTwoElements(t *testing.T) {
+//	list := []string{"apple", "orange"}
+//
+//	want := "apple and orange"
+//	got := joinWithCommas(list)
+//
+//	if got != want {
+//		t.Error(errorString(list, got, want))
+//	}
+//
+//	//t.Error("no test written yet")
+//}
+//
+//func TestThreeElements(t *testing.T) {
+//	//t.Error("no test written yet")
+//
+//	list := []string{"apple", "orange", "peach"}
+//
+//	want := "apple, orange, and peach"
+//	got := joinWithCommas(list)
+//
+//	if got != want {
+//		t.Error(errorString(list, got, want))
+//	}
+//
+//}
 
 func errorString(list []string, got string, want string) string {
 	return fmt.Sprintf("\n got: joinWithCommas(%#v) = \"%s\"\n want: \"%s\"", list, got, want)
